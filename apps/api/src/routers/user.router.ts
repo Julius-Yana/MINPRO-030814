eed5a29c5c10c411a6d9dba6a54e058327401374
juliussiregar <juliussiregar1011@gmail.com>
2024-05-14 11:41:47 +0700

    feat: update upload image

--- apps/api/src/routers/user.router.ts
+++ apps/api/src/routers/user.router.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express'
-import { getAllUsers, getUserDiscount, getUserPoint, loginUser, referralPoint, regUser, updateUser, verifyAccount } from '../controllers/user.controller'
+import { getAllUsers, getUserDiscount, getUserPoint, imageUser, loginUser, referralPoint, regUser, updateUser, verifyAccount } from '../controllers/user.controller'
 import { validateRegister } from '@/middlewares/validator'
 import { verifyToken } from '@/middlewares/verifyToken'
+import { uploader } from '@/helpers/uploader'
 
 const userRouter = Router()
 
@@ -9,6 +10,7 @@ userRouter.post('/', validateRegister, regUser)
 userRouter.post('/login', loginUser)
 userRouter.get('/',getAllUsers)
 userRouter.put('/', verifyToken, updateUser)
+userRouter.patch('/image', verifyToken, uploader("IMG", "/images").single("file"), imageUser)
 userRouter.put('/verify', verifyToken, verifyAccount)
 userRouter.post('/point', verifyToken, referralPoint)
 userRouter.get('/point', verifyToken, getUserPoint)
